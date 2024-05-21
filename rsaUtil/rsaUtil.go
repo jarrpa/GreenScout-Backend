2fa43983d0754518d3fdf99fe70effde3c2ee01c
tagciccone <tagciccone@gmail.com>
2024-05-21 15:42:33 -0500

    rsa util log coverage

--- rsaUtil/rsaUtil.go
+++ rsaUtil/rsaUtil.go
@@ -1,42 +1,67 @@
 package rsaUtil
 
 import (
+	greenlogger "GreenScoutBackend/greenLogger"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 )
 
 func GetPublicKey() string {
-	pubFile, err := os.Open(filepath.Join("rsaUtil", "login-key.pub.pem"))
-	if err != nil {
-		log.Println(err)
+	pubFile, openErr := os.Open(filepath.Join("rsaUtil", "login-key.pub.pem"))
+	if openErr != nil {
+		greenlogger.LogErrorf(openErr, "Problem opening %v", filepath.Join("rsaUtil", "login-key.pub.pem"))
+		// SLACK INTEGRATE
+		return ""
 	}
 	defer pubFile.Close()
 
-	keyBytes, _ := io.ReadAll(pubFile)
+	keyBytes, readErr := io.ReadAll(pubFile)
+	if readErr != nil {
+		greenlogger.LogErrorf(readErr, "Problem reading %v", filepath.Join("rsaUtil", "login-key.pub.pem"))
+		// SLACK INTEGRATE
+		return ""
+	}
 
 	return string(keyBytes) //Yes this only actually returns the file contents but it'll decode on the frontend
 }
 
 // Decrypts the RSA-encoded password
 func DecryptPassword(passwordEncrypted []byte) string {
-	privFile, _ := os.Open(filepath.Join("rsaUtil", "login-key.pem"))
+	privFile, openErr := os.Open(filepath.Join("rsaUtil", "login-key.pem"))
+	if openErr != nil {
+		greenlogger.LogErrorf(openErr, "Problem opening %v", filepath.Join("rsaUtil", "login-key.pem"))
+		// SLACK INTEGRATE
+		return ""
+	}
+
 	defer privFile.Close()
 
-	keyBytes, _ := io.ReadAll(privFile)
+	keyBytes, readErr := io.ReadAll(privFile)
+	if readErr != nil {
+		greenlogger.LogErrorf(readErr, "Problem reading %v", filepath.Join("rsaUtil", "login-key.pem"))
+		// SLACK INTEGRATE
+		return ""
+	}
 
 	block, _ := pem.Decode(keyBytes)
 
-	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
+	key, parseErr := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if parseErr != nil {
+		greenlogger.LogErrorf(parseErr, "Problem parsing %v", block.Bytes)
+		// SLACK INTEGRATE
+		return ""
+	}
 
-	decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, key, passwordEncrypted)
-	if err != nil {
-		println("Err: " + err.Error())
+	decrypted, decryptErr := rsa.DecryptPKCS1v15(rand.Reader, key, passwordEncrypted)
+	if decryptErr != nil {
+		greenlogger.LogErrorf(decryptErr, "Problem decrypting %v", passwordEncrypted)
+		// SLACK INTEGRATE
+		return ""
 	}
 
 	return string(decrypted)
@@ -46,13 +71,29 @@ func EncodeWithPublicKey(message string) []byte {
 	pubFile, _ := os.Open(filepath.Join("rsaUtil", "login-key.pub.pem"))
 	defer pubFile.Close()
 
-	keyBytes, _ := io.ReadAll(pubFile)
+	keyBytes, readErr := io.ReadAll(pubFile)
+	if readErr != nil {
+		greenlogger.LogErrorf(readErr, "Problem reading %v", filepath.Join("rsaUtil", "login-key.pub.pem"))
+		// SLACK INTEGRATE
+		return []byte("")
+	}
 
 	block, _ := pem.Decode(keyBytes)
 
-	key, _ := x509.ParsePKCS1PublicKey(block.Bytes)
+	key, parseErr := x509.ParsePKCS1PublicKey(block.Bytes)
 
-	result, _ := rsa.EncryptPKCS1v15(rand.Reader, key, []byte(message))
+	if parseErr != nil {
+		greenlogger.LogErrorf(parseErr, "Problem parsing %v", block.Bytes)
+		// SLACK INTEGRATE
+		return []byte("")
+	}
+
+	result, encryptErr := rsa.EncryptPKCS1v15(rand.Reader, key, []byte(message))
+	if encryptErr != nil {
+		greenlogger.LogErrorf(encryptErr, "Problem encrypting %v", message)
+		// SLACK INTEGRATE
+		return []byte("")
+	}
 
 	return result
 }
