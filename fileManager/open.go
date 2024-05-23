9c524dfd0d0c964330ab788d48d32139c8697ce2
tagciccone <tagciccone@gmail.com>
2024-05-23 11:43:37 -0500

    proper file permissions

--- fileManager/open.go
+++ fileManager/open.go
@@ -3,5 +3,13 @@ package filemanager
 import "os"
 
 func OpenWithPermissions(filepath string) (*os.File, error) {
-	return os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	os.Chmod(filepath, 0777)
+	return file, err
+}
+
+func MkDirWithPermissions(filepath string) error {
+	err := os.Mkdir(filepath, os.ModePerm)
+	os.Chmod(filepath, 0777)
+	return err
 }
