dd4b89f413838c361a611f8e98fe65638e05a06b
Jose A. Rivera <jarrpa@redhat.com>
2024-10-27 09:15:47 -0500

    make the CORS domain configurable
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- setup/setup.go
+++ setup/setup.go
@@ -45,6 +45,15 @@ func TotalSetup(inTesting bool) {
 		panic(err)
 	}
 
+	// Set frontend domain to be used for CORS
+	if configs.FrontendDomain == "" {
+		if inTesting {
+			configs.FrontendDomain = constants.DefaultFrontendDomain
+		} else {
+			panic("Please configure a FrontendDomain when running in production!")
+		}
+	}
+
 	// aaaa
 	if configs.RuntimeDirectory == "" {
 		configs.RuntimeDirectory = filepath.Join(workingDir, constants.DefaultRuntimeDirectory)
