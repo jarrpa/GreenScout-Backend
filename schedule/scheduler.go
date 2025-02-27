3d7fba9da1f16ac000ad4e9bc23c2b0e19841b00
Jose A. Rivera <jarrpa@gmail.com>
2025-02-27 13:25:11 -0600

    check your schema
    Signed-off-by: Jose A. Rivera <jarrpa@gmail.com>

--- schedule/scheduler.go
+++ schedule/scheduler.go
@@ -103,7 +103,7 @@ func AddIndividualSchedule(name string, nameIsUUID bool, ranges ScoutRanges) {
 
 		rangeString = string(newRangeBytes)
 
-		_, resultErr := scoutDB.Exec("update individuals set ranges = ? where uuid = ?", rangeString, uuid)
+		_, resultErr := scoutDB.Exec("update individuals set schedule = ? where uuid = ?", rangeString, uuid)
 		if resultErr != nil {
 			greenlogger.LogErrorf(resultErr, "Problem executing sql command %v with args %v", "update individuals set ranges = ? where uuid = ?", []any{rangeString, uuid})
 		}
